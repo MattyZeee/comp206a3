23d1ca3a548f9ab1e38ce6fa2aad9a6edd16af74
Matt Zwalsky <mattzwalsky@matt-zwalskys-macbookpro55.local>
2015-03-23 21:05:01 -0500

    finished list.h

---
+++ list.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BOOLEAN int
+#define TRUE 1
+#define FALSE 0
+
+void add(int a);
+void prettyPrint();
+BOOLEAN delete (int a);
+		
