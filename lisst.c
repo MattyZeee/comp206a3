ba26b9cd6c15fc54e9b06026e90a1d64504f12d9
Matt Zwalsky <mattzwalsky@matt-zwalskys-macbookpro55.local>
2015-03-23 21:01:22 -0500

    finished list.c

--- lisst.c
+++ lisst.c
@@ -50,4 +50,42 @@ void prettyPrint()      //traverse the list starting at head and print values.
         }
 }
 
+struct NUMBER_NODE *find(int a) //find method is hidden (not declared in list.h file)
+{
+        struct NUMBER_NODE *p = head;
+        struct NUMBER_NODE *node_before_p = head;
+        while (p!= NULL)                //traverse the linked list
+        {
+                if (p->value == a)      //if a value matches then..
+                {
+                        if (p == head)
+                        {
+                                head = p->next; //if it is the first node, have the head point at the second node
+                        }
+                        else
+                        {
+                                node_before_p->next = p->next;  //else have previous node point to next node removing the current node from the linked list
+                                //free (p);     //wasnt sure if i should free the node. It didnt specify to do this in the assignment guidelines
+                                return p;       //return a pointer pointing to the node
+                         }
+                }
+                else
+                        {
+                                node_before_p = p;      //else continue traversing the list to see if next node contains matching value
+                                p = p -> next;
+                        }
+        }
+        return NULL;                    //no node contained matching value so return NULL
+}
+
+
+
+BOOLEAN delete (int a)
+{
+        struct NUMBER_NODE *ptr = find(a);
+        if (ptr != NULL) return EXIT_SUCCESS;   //I chose to return EXIT_SUCCESS AND EXIT_FAILURE... I could have returned TRUE and FALSE
+        else return EXIT_FAILURE;
+}
+
+
 
